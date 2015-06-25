9b81634097b0f03f9cadc1b5a4ff9f554f2d3e30
Louis Ewington <lewington3348@gmail.com>
2015-06-25 09:18:24 -0400

    save

--- narwhals.py
+++ narwhals.py
@@ -6,27 +6,37 @@ from utils import ask
 intro = c.magenta + '''LETS TEST HOW FABILOOS U ARE, YYYYAAAAAAAYYYYY!!!''' + c.reset
 
 def q1():
-    answer = ask(c.orange + 'WHAT COLOR WERE THE FABILOOS UNICORNS?' + c.reset) 
-    if answer =='pink':
+    answer = ask(c.orange + 'What are the swaggiest animals ever?' + c.reset) 
+    if answer =='narwhals':
         print('so nice and purty')
         return True
     print('incorect :(======')
     return False
 
 def q2(): 
-    answer = ask(c.magenta + 'WHAT WERE THE UNICORNS DANCING ON?')
-    if answer.startswith ('rainbows'):
-        print('also so nice an purty')
+    answer = ask(c.magenta + 'are narwhals swaggy?')
+    if answer == 'yes':
+        print('yes')
         return True
     print('u make me sad :(=======')    
     return False
 
 def q3():
-    answer =ask(c.magenta + 'use one word to describe the texture of their magical fur.')
-    if answer == 'smiles':
+    answer =ask(c.magenta + 'should narwhals be the dominant species of the world?')
+    if answer == 'yes':
         print('yyyeeeeeaaaahhhh')
         return True
     print('eradicate from the world, }:(========')
     return False
 
-questions = [q1,q2,q3]
+def q4():
+    answer =ask(c.magenta + 'what is the average life span of a narwhal in years?')
+    if answer == '25-30':
+        print('yes')
+        return True
+    print('no')
+    return False
+
+
+
+questions = [q1,q2,q3,q4]
